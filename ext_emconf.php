91210603c55da81df6e1c3869264e89d413084ab
Meelis Karulin <karulin@mediatis.de>
2023-08-04 11:19:58 +0200

    wip

---
+++ ext_emconf.php
@@ -0,0 +1,21 @@
+<?php
+
+$EM_CONF[$_EXTKEY] = [
+    'title' => 'Digital Marketing Framwork - Distributor - Csv',
+    'description' => 'Save form data to a CSV file',
+    'category' => 'be',
+    'author' => 'Meelis Karulin',
+    'author_email' => 'info@mediatis.de',
+    'author_company' => 'Mediatis AG',
+    'state' => 'stable',
+    'version' => '1.0.0',
+    'constraints' => [
+        'depends' => [
+            'typo3' => '11.5.0-11.5.99',
+        ],
+        'conflicts' => [
+        ],
+        'suggests' => [
+        ],
+    ],
+];
