e885ff72d5c63e474b9f63e6cd8b8bc5bd02cd2e
Meelis Karulin <karulin@mediatis.de>
2023-09-08 20:33:00 +0200

    wip

--- Classes/ConfigurationDocument/Storage/EventListener/StaticConfigurationDocumentEventListener.php
+++ Classes/ConfigurationDocument/Storage/EventListener/StaticConfigurationDocumentEventListener.php
@@ -2,7 +2,6 @@
 
 namespace DigitalMarketingFramework\Typo3\Distributor\Csv\ConfigurationDocument\Storage\EventListener;
 
-use DigitalMarketingFramework\Typo3\Core\ConfigurationDocument\Event\ConfigurationDocumentMetaDataUpdateEvent;
 use DigitalMarketingFramework\Typo3\Core\ConfigurationDocument\Storage\EventListener\StaticConfigurationDocumentEventListener as OriginalStaticConfigurationDocumentEventListener;
 
 class StaticConfigurationDocumentEventListener extends OriginalStaticConfigurationDocumentEventListener
