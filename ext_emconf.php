75c32736bb4833c3b3da3ad1c5dab112ca815bfd
s-ude <ude@mediatis.de>
2023-10-12 16:23:44 +0200

    [TASK] coding standards checks (#2)

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,14 +1,14 @@
 <?php
 
 $EM_CONF[$_EXTKEY] = [
-    'title' => 'Digital Marketing Framwork - Distributor - Csv',
+    'title' => 'Digital Marketing Framwork - Distributor - CSV',
     'description' => 'Save form data to a CSV file',
     'category' => 'be',
     'author' => 'Meelis Karulin',
     'author_email' => 'info@mediatis.de',
     'author_company' => 'Mediatis AG',
     'state' => 'stable',
-    'version' => '1.0.1',
+    'version' => '1.0.2',
     'constraints' => [
         'depends' => [
             'typo3' => '11.5.0-12.4.99',
